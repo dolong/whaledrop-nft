9b26b05bcac9c95cf180b2da426f6f7499550644
Long Do <longdo@Longs-MacBook-Pro.local>
2021-04-03 19:20:08 -0400

    Treasure Land Combo Analysis

--- alpacaTreasureLand.js
+++ alpacaTreasureLand.js
@@ -65,18 +65,133 @@ function formatResponse(response) {
     name = alpacaData.name
     attributes = alpacaData.attributes
     formattedAttributes = ""
+    moreAttrs = getAlpaca(alpacaData)
     for (i in attributes) {
-        formattedAttributes += `<tr><td>${attributes[i].trait_type}</td><td>${attributes[i].value}</td></tr>`
-    };
+        switch (attributes[i].trait_type) {
+            case "energy":
+                formattedAttributes += `<tr><td>Energy</td><td>${attributes[i].value}</td></tr>`
+                break;
+            case "breeding fee reset at":
+                $(`#alpacaBreedingTime`).html(traits[i].value)
+                formattedAttributes += `<tr><td>Breeding Time</td><td>${attributes[i].value}</td></tr>`
+                break;
+            case "generation":
+                formattedAttributes += `<tr><td>Gen</td><td>${attributes[i].value}</td></tr>`
+                break;
+        }
+    }
+
     newData = `<tbody><td>
- <tr>${name}</tr>
  ${formattedAttributes}
  </td></tbody>`
 
     var newAlpaca = document.createElement("table");
-    newAlpaca.innerHTML = newData;
+    newAlpaca.innerHTML = newData + moreAttrs;
 
     return newAlpaca
 }
 
+
+
+//Get More Alpaca Info
+function getAlpaca(alpacaJson) {
+
+    //Sort the Important traits
+    const traits = alpacaJson.attributes
+    let attrs = ""
+
+    let fire_trait_count = 0
+    let leaf_trait_count = 0
+    let lightning_trait_count = 0
+    for (var i in traits) {
+        switch (traits[i].trait_type) {
+            case "accent_color":
+                attrs += `<b>Accent Color:</b> ${traits[i].value}`
+                break;
+            case "accent_color":
+                attrs += `<br><b>Accent Color:</b> ${traits[i].value}`
+                break;
+            case "base_color":
+                attrs += `<br><b>Base Color:</b> ${traits[i].value}`
+                break;
+            case "ears":
+                attrs += `<br><b>Ears:</b> ${traits[i].value}`
+                break;
+            case "eye_color":
+                attrs += `<br><b>Eye Color:</b> ${traits[i].value}`
+                break;
+            case "eye_shape":
+                attrs += `<br><b>Eye Shape:</b> ${traits[i].value}`
+                break;
+            case "hairs":
+                attrs += `<br><b>Hair:</b> ${traits[i].value}`
+                break;
+            case "highlight_color":
+                attrs += `<br><b>Highlight Color:</b> ${traits[i].value}`
+                break;
+            case "mouth":
+                attrs += `<br><b>Mouth:</b> ${traits[i].value}`
+                break;
+            case "pattern":
+                attrs += `<br><b>Pattern:</b> ${traits[i].value}`
+                break;
+            case "tail":
+                attrs += `<br><b>Tail:</b> ${traits[i].value}`
+                break;
+        }
+
+        const fireTraits = [
+            "passion", "orange", "burnt sienna", "christine",
+            "bonfire", "torch",
+            "crab pliers", "feathers",
+            "onfire", "chad",
+            "spark", "spaark",
+            "blaze", "inferno"
+        ]
+        const leafTraits = [
+            "grass", "spring green", "jade", "downy",
+            "fox", "spectre",
+            "batwings",
+            "mulan", "cotton candy", "wind wings",
+            "leaf"
+        ]
+        const lightningTraits = [
+            "blue violet", "magenta", "studio",
+            "lightning", "playboy",
+            "deniro",
+            "saber", "crack"
+        ]
+        if (fireTraits.indexOf(traits[i].value) > -1) {
+            attrs += `<span style="color:red"> FIRE</span>`
+            fire_trait_count++
+        }
+        if (leafTraits.indexOf(traits[i].value) > -1) {
+            attrs += `<span style="color:green"> LEAF</span>`
+            leaf_trait_count++
+        }
+        if (lightningTraits.indexOf(traits[i].value) > -1) {
+            attrs += `<span style="color:orange"> LIGHTNING</span>`
+            lightning_trait_count++
+        }
+
+
+    }
+
+    if (fire_trait_count == 5)
+        attrs += `<h4 style="color: red">FULL FIRE COMBO</h4>`
+    else if (leaf_trait_count == 5)
+        attrs += `<h4 style="color: green">FULL LEAF COMBO</h4>`
+    else if (lightning_trait_count == 5)
+        attrs += `<h4 style="color: orange">FULL LIGHTNING COMBO</h4>`
+    else {
+        attrs +=
+            `<br><p>
+                <span style="color:red">${fire_trait_count}/5 FIRE</span>
+                <span style="color:green">${leaf_trait_count}/5 LEAF</span>
+                <span style="color:orange">${lightning_trait_count}/5 LIGHTNING</span>
+            </p>`
+    }
+
+    return attrs;
+}
 chrome.extension.sendRequest(alpacaObjects);
