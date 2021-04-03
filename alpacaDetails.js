c63fbc4f721d82feaeb9e37b65075e54d195ba63
Long Do <longdo@Longs-MacBook-Pro.local>
2021-04-03 18:30:06 -0400

    Elements for alpaca details

--- alpacaDetails.js
+++ alpacaDetails.js
@@ -41,42 +41,97 @@ function getAlpaca(alpacaJson) {
     const traits = alpacaJson.attributes
     let attrs = ""
 
+    let fire_trait_count = 0
+    let leaf_trait_count = 0
+    let lightning_trait_count = 0
     for (var i in traits) {
         switch (traits[i].trait_type) {
             case "accent_color":
-                attrs += `<br><b>Accent Color:</b> ${traits[i].value}<br>`
+                attrs += `<b>Accent Color:</b> ${traits[i].value}`
                 break;
             case "accent_color":
-                attrs += `<br><b>Accent Color:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Accent Color:</b> ${traits[i].value}`
                 break;
             case "base_color":
-                attrs += `<b>Base Color:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Base Color:</b> ${traits[i].value}`
                 break;
             case "ears":
-                attrs += `<b>Ears:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Ears:</b> ${traits[i].value}`
                 break;
             case "eye_color":
-                attrs += `<b>Eye Color:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Eye Color:</b> ${traits[i].value}`
                 break;
             case "eye_shape":
-                attrs += `<b>Eye Shape:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Eye Shape:</b> ${traits[i].value}`
                 break;
             case "hairs":
-                attrs += `<b>Hair:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Hair:</b> ${traits[i].value}`
                 break;
             case "highlight_color":
-                attrs += `<b>Highlight Color:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Highlight Color:</b> ${traits[i].value}`
                 break;
             case "mouth":
-                attrs += `<b>Mouth:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Mouth:</b> ${traits[i].value}`
                 break;
             case "pattern":
-                attrs += `<b>Pattern:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Pattern:</b> ${traits[i].value}`
                 break;
             case "tail":
-                attrs += `<b>Tail:</b> ${traits[i].value}<br>`
+                attrs += `<br><b>Tail:</b> ${traits[i].value}`
                 break;
         }
+
+        const fireTraits = [
+            "passion", "orange", "burnt sienna", "christine",
+            "bonfire", "torch",
+            "crab pliers", "feathers",
+            "onfire", "chad",
+            "spark", "spaark",
+            "blaze", "inferno"
+        ]
+        const leafTraits = [
+            "grass", "spring green", "jade", "downy",
+            "fox", "spectre",
+            "batwings",
+            "mulan", "cotton candy", "wind wings",
+            "leaf"
+        ]
+        const lightningTraits = [
+            "blue violet", "magenta", "studio",
+            "lightning", "playboy",
+            "deniro",
+            "saber", "crack"
+        ]
+        if (fireTraits.indexOf(traits[i].value) > -1) {
+            attrs += `<span style="color:red"> FIRE</span>`
+            fire_trait_count++
+        }
+        if (leafTraits.indexOf(traits[i].value) > -1) {
+            attrs += `<span style="color:green"> LEAF</span>`
+            leaf_trait_count++
+        }
+        if (lightningTraits.indexOf(traits[i].value) > -1) {
+            attrs += `<span style="color:orange"> LIGHTNING</span>`
+            lightning_trait_count++
+        }
+
+
     }
+
+    if (fire_trait_count == 5)
+        attrs += `<h4 style="color: red">FULL FIRE COMBO</h4>`
+    else if (leaf_trait_count == 5)
+        attrs += `<h4 style="color: green">FULL LEAF COMBO</h4>`
+    else if (lightning_trait_count == 5)
+        attrs += `<h4 style="color: orange">FULL LIGHTNING COMBO</h4>`
+    else {
+        attrs +=
+            `<br><p>
+                <span style="color:red">${fire_trait_count}/5 FIRE</span>
+                <span style="color:green">${leaf_trait_count}/5 LEAF</span>
+                <span style="color:orange">${lightning_trait_count}/5 LIGHTNING</span>
+            </p>`
+    }
+
     return attrs;
 }
